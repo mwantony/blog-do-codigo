74e0d1f138a06ee3a911d432f33180e92354df6d
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-07 15:07:12 -0300

    cap 3 vid 4 finalizado

--- src/usuarios/usuarios-rotas.js
+++ src/usuarios/usuarios-rotas.js
@@ -1,14 +1,23 @@
-const usuariosControlador = require('./usuarios-controlador');
-const passport = require('passport')
-module.exports = app => {
+const usuariosControlador = require("./usuarios-controlador");
+const middlewaresAutenticacao = require('./middlewares-autenticacao')
+const passport = require("passport");
+module.exports = (app) => {
   app
-    .route('/usuario/login')
-    .post(passport.authenticate('local', {session: false}), usuariosControlador.login)
+    .route("/usuario/login")
+    .post(
+      middlewaresAutenticacao.local,
+      usuariosControlador.login
+    );
 
   app
-    .route('/usuario')
+    .route("/usuario")
     .post(usuariosControlador.adiciona)
     .get(usuariosControlador.lista);
 
-  app.route('/usuario/:id').delete(passport.authenticate('bearer', {session: false}), usuariosControlador.deleta);
+  app
+    .route("/usuario/:id")
+    .delete(
+      passport.authenticate("bearer", { session: false }),
+      usuariosControlador.deleta
+    );
 };
