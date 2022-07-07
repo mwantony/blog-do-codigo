f1b1b1db995907ede1c3259d7a7c887edde5f7b9
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-07 14:38:08 -0300

    cap 3 vid 3 finalizado

--- src/usuarios/estrategias-autenticacao.js
+++ src/usuarios/estrategias-autenticacao.js
@@ -1,6 +1,8 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+const BearerStrategy = require('passport-http-bearer').Strategy
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const Usuario = require("./usuarios-modelo");
 const { InvalidArgumentError } = require('../erros')
 function verificaUsuario(usuario) {
@@ -35,3 +37,17 @@ passport.use(
     }
   )
 );
+
+passport.use(
+  new BearerStrategy(
+    async (token, done) => {
+      try {
+        const payload = jwt.verify(token, process.env.CHAVE_JWT)
+        const usuario = await Usuario.buscaPorId(payload.id)
+        done(null, usuario)
+      } catch(erro) {
+        done(erro)
+      }
+    }
+  )
+)
